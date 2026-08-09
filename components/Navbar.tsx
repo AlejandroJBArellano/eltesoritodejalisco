a29d7b3e3cdd1fcefbf6c035625213e92e2fb3df
Alejandro Arellano <jesusarellano334@gmail.com>
2026-08-09 11:33:00 -0600

    style: hide navbar during print operations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-dark border-b border-border text-white sticky top-0 z-40">
+    <nav className="bg-dark border-b border-border text-white sticky top-0 z-40 no-print">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center gap-4">
           {/* Logo */}
